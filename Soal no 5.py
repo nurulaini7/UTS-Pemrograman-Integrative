8a3257348fcdf8d926ca4425c0c0b76cd697fa59
Nurul Aini <119827418+nurulaini7@users.noreply.github.com>
2024-03-25 14:41:55 +0700

    Update Soal no 5.py

--- Soal no 5.py
+++ Soal no 5.py
@@ -4,12 +4,10 @@ Nim : 221402005
 
 """
 
-import os
-
-file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-with open(file_path, 'r') as file:
+with open("UTS/input.txt", "r") as file:
     numbers = [int(line.strip()) for line in file.readlines()]
 
 total_sum = sum(numbers)
 formatted_sum = "{:,.3f}".format(total_sum)
+
 print(formatted_sum)
